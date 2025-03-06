82a53412b772e9b05f36912ed51f0346dd56e234
GediminasM <gedmocss@gmail.com>
2025-03-06 19:03:56 +0200

    Initial commit

---
+++ src/main/java/lt/projectx/producttask3/ProductTask3Application.java
@@ -0,0 +1,13 @@
+package lt.projectx.producttask3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductTask3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductTask3Application.class, args);
+	}
+
+}
---
+++ src/test/java/lt/projectx/producttask3/ProductTask3ApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.projectx.producttask3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductTask3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
